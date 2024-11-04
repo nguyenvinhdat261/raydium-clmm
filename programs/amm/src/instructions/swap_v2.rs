b3520c995badc3f1941f30eec58b94b3629cfdf2
nguyenvinhdat261 <122195081+nguyenvinhdat261@users.noreply.github.com>
2024-11-04 08:35:37 +0700

    Update swap_v2.rs

--- programs/amm/src/instructions/swap_v2.rs
+++ programs/amm/src/instructions/swap_v2.rs
@@ -88,8 +88,6 @@ pub fn exact_internal_v2<'c: 'info, 'info>(
 
     let block_timestamp = solana_program::clock::Clock::get()?.unix_timestamp as u64;
 
-    let amount_0;
-    let amount_1;
     let zero_for_one;
     let swap_price_before;
 
@@ -126,18 +124,22 @@ pub fn exact_internal_v2<'c: 'info, 'info>(
             ErrorCode::InvalidInputPoolVault
         );
 
+        let mut tickarray_bitmap_extension = None;
+        let tick_array_states = &mut VecDeque::new();
 
-        #[cfg(feature = "enable-log")]
-        msg!(
-            "exact_swap_internal, is_base_input:{}, amount_0: {}, amount_1: {}",
-            is_base_input,
-            amount_0,
-            amount_1
-        );
-        require!(
-            amount_0 != 0 && amount_1 != 0,
-            ErrorCode::TooSmallInputOrOutputAmount
-        );
+        let tick_array_bitmap_extension_key = TickArrayBitmapExtension::key(pool_state.key());
+        for account_info in remaining_accounts.into_iter() {
+            if account_info.key().eq(&tick_array_bitmap_extension_key) {
+                tickarray_bitmap_extension = Some(
+                    *(AccountLoader::<TickArrayBitmapExtension>::try_from(account_info)?
+                        .load()?
+                        .deref()),
+                );
+                continue;
+            }
+            tick_array_states.push_back(AccountLoad::load_data_mut(account_info)?);
+        }
+
     }
     let (token_account_0, token_account_1, vault_0, vault_1, vault_0_mint, vault_1_mint) =
         if zero_for_one {
